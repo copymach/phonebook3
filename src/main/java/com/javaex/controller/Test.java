40714827b727e4d5ee105cfc202f1ea6707534dd
copymachine <copymach@naver.com>
2022-01-18 11:51:52 +0900

    스프링을 사용한 폰북3

---
+++ src/main/java/com/javaex/controller/Test.java
@@ -0,0 +1,17 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@controller
+public class Test {
+
+	public void TestPrint() {
+		
+	}
+	
+	public void TestPrint2() {
+		
+	}
+	
+}
