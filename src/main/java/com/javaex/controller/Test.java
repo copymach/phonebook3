104be48574ae73cced1fe041d6b658de6da15091
copymachine <copymach@naver.com>
2022-01-18 22:03:05 +0900

    폰북3 수정 삭제 기능 완성

--- src/main/java/com/javaex/controller/Test.java
+++ src/main/java/com/javaex/controller/Test.java
@@ -1,17 +1,39 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
-@controller
+@Controller
+@RequestMapping(value="/board")
 public class Test {
 
-	public void TestPrint() {
+//	필드
+	
+//	생성자
+	
+//	메서드
+
+//	메소드일반
+	@RequestMapping (value="/list", method= {RequestMethod.GET, RequestMethod.POST})
+	public String TestPrint() {
+		System.out.println("TestPrint");
 		
+		return "WEB-INF/views/Test.jsp";
 	}
 	
-	public void TestPrint2() {
+	@RequestMapping (value="/writeForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String TestPrint2() {
+		System.out.println("TestPrint2");
 		
+		return "WEB-INF/views/Test.jsp";
 	}
 	
+	
+	@RequestMapping (value="/addForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String TestPrint3() {
+		System.out.println("TestPrint3");
+		
+		return "WEB-INF/views/Test.jsp";
+	}
 }
